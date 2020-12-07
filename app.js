02ed771f5c1fd7b24546bcddcb4c8e91b562ecc9
petermagnum <pit.irarrazabal@gmail.com>
2020-12-07 02:02:02 -0300

    con boton actualizar funcionando

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const app = new Vue({
     nuevoProducto: "",
     nuevaDescripcion: "",
     nuevoPrecio: "",
+
+    editProducto:"",
+    editDescripcion: "",
+    editPrecio: "",
   },
   methods: {
     agregarProducto: function () {
       if (this.nuevoProducto == "" || this.nuevaDescripcion == "" || this.nuevoPrecio <= 0) {
         console.log("Llene todos los campos y un precio superior a 0");
-        alert("Llene todos los campos y un precio superior a 0")
+        alert("Llene todos los campos y un precio superior a 0")      
       } else {
         this.Productos.push({
           nombre: this.nuevoProducto,
@@ -23,11 +27,33 @@ const app = new Vue({
         this.nuevoProducto = "";
         this.nuevaDescripcion = "";
         this.nuevoPrecio = "";
+
+        this.editProducto="";
+        this.editDescripcion= "";
+        this.editPrecio= "";
+
       }
     },
 
+    setearProducto: function (index) {
+
+        this.editProducto =this.Productos[index].nombre;
+        this.editDescripcion= this.Productos[index].descripcion;
+        this.editPrecio= this.Productos[index].precio;
+      
+      },
+
     editarProducto: function (index) {
-      this.Productos[index].estado = true;
+
+
+      this.Productos[index].nombre = this.editProducto;
+      this.Productos[index].descripcion = this.editDescripcion;
+      this.Productos[index].precio = this.editPrecio;
+
+      this.editProducto="";
+      this.editDescripcion= "";
+      this.editPrecio= "";
+
     },
     eliminarProducto: function (index) {
       this.Productos.splice(index, 1);
